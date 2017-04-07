ef6341283c03108b3e9508b233b25e0dfc0bd28b
Treeisu <cherryjiangbiao@qq.com>
2017-04-07 18:03:22 +0800

    新建PushInfo实体类

---
+++ CampusOfCircle Maven Webapp/src/main/java/org/jiang/COC/model/PushInfo.java
@@ -0,0 +1,133 @@
+package org.jiang.COC.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ * 用户注信息表
+ * 注册时需要填写信息
+ * @author Cherry
+ *
+ */
+@Entity
+@Table(name="tb_pushInfo")
+public class PushInfo implements Serializable {
+	@Transient
+	private static final long serialVersionUID = 4048529031442266867L;
+	@Transient
+	private PushInfo initPushInfo;//用来给业务层处理转发的原信息，加上一个@Transient数据库则不会产生列
+	
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "wbId", unique = true, nullable = false)
+	private long wbId;
+	@Column(name = "wbAuthorId",length=20)
+	private long wbAuthorId;//这个字段用来存储转发的原始Id
+	@Column(name = "userId",length=20)
+	private long userId;
+	@Column(name = "userNickName",length=20)
+    private String userNickName;
+	@Column(name = "wbImage",length=300)
+    private String wbImage;
+	@Column(name = "wbTextContent",length=500)
+    private String wbTextContent;
+    @Column(name = "wbPushDate",length=20)
+    private Date wbPushDate;
+	
+    
+    public PushInfo getInitPushInfo() {
+		return initPushInfo;
+	}
+	public void setInitPushInfo(PushInfo initPushInfo) {
+		this.initPushInfo = initPushInfo;
+	}
+	public long getWbId() {
+		return wbId;
+	}
+	public void setWbId(long wbId) {
+		this.wbId = wbId;
+	}
+	public long getWbAuthorId() {
+		return wbAuthorId;
+	}
+	public void setWbAuthorId(long wbAuthorId) {
+		this.wbAuthorId = wbAuthorId;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getUserNickName() {
+		return userNickName;
+	}
+	public void setUserNickName(String userNickName) {
+		this.userNickName = userNickName;
+	}
+	public String getWbImage() {
+		return wbImage;
+	}
+	public void setWbImage(String wbImage) {
+		this.wbImage = wbImage;
+	}
+	public String getWbTextContent() {
+		return wbTextContent;
+	}
+	public void setWbTextContent(String wbTextContent) {
+		this.wbTextContent = wbTextContent;
+	}
+	public Date getWbPushDate() {
+		return wbPushDate;
+	}
+	public void setWbPushDate(Date wbPushDate) {
+		this.wbPushDate = wbPushDate;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public PushInfo(PushInfo initPushInfo, long wbId, long wbAuthorId,
+			long userId, String userNickName, String wbImage,
+			String wbTextContent, Date wbPushDate) {
+		super();
+		this.initPushInfo = initPushInfo;
+		this.wbId = wbId;
+		this.wbAuthorId = wbAuthorId;
+		this.userId = userId;
+		this.userNickName = userNickName;
+		this.wbImage = wbImage;
+		this.wbTextContent = wbTextContent;
+		this.wbPushDate = wbPushDate;
+	}
+	public PushInfo() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "PushInfo [initPushInfo=" + initPushInfo + ", wbId=" + wbId
+				+ ", wbAuthorId=" + wbAuthorId + ", userId=" + userId
+				+ ", userNickName=" + userNickName + ", wbImage=" + wbImage
+				+ ", wbTextContent=" + wbTextContent + ", wbPushDate="
+				+ wbPushDate + "]";
+	}
+   
+	
+   
+
+	
+	
+	
+	
+    
+
+}
